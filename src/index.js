a8801cf387c1a94d1e3dec3a4ae54bcf6c0f49da
Magnun A V F <magnun.furtado@azion.com>
2023-09-18 10:44:48 -0300

    feat: Add sys info message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const os = require("os");
+
+console.log("Hello!");
+
+const message = `
+### System Infos ###
+* arch: ${os.arch()}
+* platform: ${os.platform()}
+* release: ${os.release()}
+* kernel version: ${os.version()}
+* mem: ${os.totalmem()}
+* cpus: ${JSON.stringify(os.cpus())}
+`;
+
+console.log(`\n${message}\n`);
